e9d1ff38e5a9cfd032741a8f44a4dccdf766b7d6
NithishRana17 <nithishrana17@gmail.com>
2024-08-22 19:11:19 +0530

    React apps seesion 22-08-24

---
+++ react-apps/app2/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>LOGIN HERE</h2>
+      <Login />
+    </div>
+  );
+}
+export default App;
